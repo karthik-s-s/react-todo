f99257410b55bde419ebfaf299c65070241397b2
karthik sudhakar <karthiksudhakar999@gmail.com>
2022-06-24 12:48:07 +0530

    success

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
+
 function App() {
+  const [toDos,setTodos] = useState([])
+  const [toDo,setToDo] = useState('')
+
+  const deleteTask = (index)=> {
+
+    var ask = window.confirm("Do you want to delete the task?");
+    if (ask) {
+      const test = [...toDos];
+        test.splice(index, 1);
+
+        setTodos(test)
+    }else {
+      console.log("Don't delete");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        {/* <h2>Whoop, it's Wednesday 🌝 ☕ </h2> */}
+      </div>
+      <div className="input">
+        <input value={toDo} onChange={(event)=>setToDo(event.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=>setTodos([...toDos,{id:Date.now(), text : toDo, status:false}])} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+        {
+          toDos.map((value,index)=>{
+            return(
+        <div className="todo">
+          <div className="left">
+            <input onChange={(e)=>{
+              console.log(e.target.checked);//boolean check box value
+              console.log(value);//full object of map todo
+              console.log(index);
+              setTodos(toDos.filter(value2=>{
+                if(value2.id === value.id){
+                  value2.status = e.target.checked
+                  
+                }
+                return value2
+              }))
+              
+            }} type="checkbox" name="" id="" />
+            <p>{value.text}</p>
+          </div>
+          <div className="right">
+            <i onClick={()=>deleteTask(index)} className="fas fa-times"></i>
+          </div>
+        </div>
+            ) 
+        })
+}
+{
+  toDos.map((value)=>{
+    if(value.status){
+      return(<h1>{value.text}</h1> )
+    }
+    return null;
+  })
+}
+      </div>
     </div>
   );
 }
 
 export default App;
+
